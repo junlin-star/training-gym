5517517c282e41849d8d0464202ce3b27410fcd5
Joy Liu <joy@modal.com>
2026-06-02 20:42:27 +0000

    :fire: Remove add kimi

--- modal_training_gym/common/models/__init__.py
+++ modal_training_gym/common/models/__init__.py
@@ -13,7 +13,6 @@ from .qwen3_8b import Qwen3_8B
 from .qwen3_14b import Qwen3_14B
 from .qwen3_30b import Qwen3_30B
 from .qwen3_32b import Qwen3_32B
-from .kimi_k2_5 import Kimi_K2_5
 from .qwen3_6_35b import Qwen3_6_35B
 
 __all__ = [
@@ -28,7 +27,6 @@ __all__ = [
     "Qwen3_14B",
     "Qwen3_30B",
     "Qwen3_32B",
-    "Kimi_K2_5",
     "ToolCall",
     "parse_qwen3_response",
     "Qwen3_6_35B",
