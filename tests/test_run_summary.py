567b7e9315202425f8defec96ffae2c1d97e4660
Rhea Malik <rhea.malik@modal.com>
2026-07-21 14:07:24 +0000

    format

--- tests/test_run_summary.py
+++ tests/test_run_summary.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
 import pytest
-from pydantic import ValidationError
 
 from modal_training_gym.common import run_summary as run_summary_module
 from modal_training_gym.common.run_summary import (
-    RunSummary,
     build_run_summaries,
     build_run_summary,
 )
